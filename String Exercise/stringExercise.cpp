71d103fc8df50e5e1f3533c2f7849b7c3c1bc0d2
harrisonrazan <harrisonrazan@gmail.com>
2018-09-21 16:46:42 -0400

    solution for palindrome

--- String Exercise/stringExercise.cpp
+++ String Exercise/stringExercise.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 ifstream fin;
 
-fin.open("input.txt");
+
 
 void palindrome();
 void sortFile();
@@ -15,32 +15,71 @@ void search();
 void checkNumber();
 
 int main() {
-  palindrome();
-  sortFile();
-  removeVowe();
-  search();
-  checkNumber();
+	palindrome();
+	sortFile();
+	removeVowel();
+	search();
+	checkNumber();
 
-  cout<<"If this prints out, chances are your code works :D"<<endl;
-  return 0;
+	cout << "If this prints out, chances are your code works :D" << endl;
+	system("pause");
+	return 0;
 }
 
-void palindrome(){
+void palindrome() {
+	ofstream fout;
+	string line, temp;
+	bool check = false;
+
+	fin.open("input.txt");
+	fout.open("palindrome.txt");
+
+	while (!fin.eof()) {
+		getline(fin, line);
+		temp = line;
+
+		for (int i = 0; i < temp.length(); i++) {
+			if (temp[i] >= 'A' && temp[i] <= 'Z')
+				temp[i] = tolower(temp[i]);
+			else if (temp[i] >= 'a' && temp[i] <= 'z')
+				temp[i] = temp[i];
+			else if (temp[i] >= '0' && temp[i] <= '9')
+				temp[i] = temp[i];
+			else {
+				temp.erase(i, 1);
+				i = i - 1;
+			}
+		}
+
+		for (int i = 0; i < (temp.length() / 2); i++) {
+			if (temp[i] != temp[temp.length() - (i + 1)]) {
+				check = false;
+				break;
+			}
+			else
+				check = true;
+		}
+
+		if (check == true)
+			fout << temp << endl;
+	}
+
+	fin.close();
+	fout.close();
+}
+
+void sortFile() {
 
 }
 
-void sortFile(){
+void removeVowel() {
 
 }
 
-void removeVowel(){
+void search() {
 
 }
 
-void search(){
+void checkNumber() {
 
-}
-
-void checkNumber(){
-  
 }
