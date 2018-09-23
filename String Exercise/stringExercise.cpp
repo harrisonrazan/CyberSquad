d417012394b79567889e56603f29e29404e8fdae
Ange Nadje <ange.j.nadje@gmail.com>
2018-09-22 20:45:53 -0400

    Added the palindrome function, let me know if you need any clarification.

--- String Exercise/stringExercise.cpp
+++ String Exercise/stringExercise.cpp
@@ -14,17 +14,35 @@ void checkNumber();
 
 int main() {
 	palindrome();
-	sortFile();
+	/*sortFile();
 	removeVowel();
 	search();
 	checkNumber();
-
+	*/
 	cout << "If this prints out, chances are your code works :D" << endl;
 	return 0;
 }
 
 void palindrome() {
+	int i = 0;
+	char word[100]; // char array to hold a user input string
+	cout << "please enter a word" << endl;
+	cin >> word;
+	int j = strlen(word) - 1; // counter that is equal to the last letter in "word"
+	while (i != j) { // compare first and last letter in "word" then each counter will step closer to the middle
+		if (word[i] == word[j]) {
+			i++;
+			j--;
+		}
+		else {
+			cout << "This word isn't a palindrome" << endl;
+			break;
+		}
+	}
+	if (i == j) {
+		cout << "This word is a palindrome" << endl;
 
+	}
 }
 
 void sortFile() {
